ae6dec691a07a730671c6b067b26481469e5402c
Shivani Panjala <shivanimadhav4@gmail.com>
2025-10-22 12:25:42 +0530

    feat: table annotations

--- src/main/java/com/example/hospitalManagement/entity/Patient.java
+++ src/main/java/com/example/hospitalManagement/entity/Patient.java
@@ -1,9 +1,6 @@
 package com.example.hospitalManagement.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -14,6 +11,23 @@ import java.time.LocalDate;
 @ToString
 @Getter
 @Setter
+// Maps this entity to the database table named "patient_tbl"
+@Table(
+        name = "patient_tbl",
+        // Define unique constraints to prevent duplicate records for these fields
+        uniqueConstraints = {
+                // Ensures that no two patients can have the same email
+                @UniqueConstraint(name = "unique_patient_email", columnNames = {"email"}),
+                // Ensures that no two patients can have the same combination of name and birthDate
+                @UniqueConstraint(name ="unique_patient_name_birthdate", columnNames = {"name", "birthDate"})
+        },
+        // Define indexes to improve query performance
+        indexes = {
+                // Creates an index on the birthDate column for faster searches and filtering by date
+                @Index(name ="idx_patient_birth_date", columnList = "birthDate")
+        }
+
+)
 public class Patient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
