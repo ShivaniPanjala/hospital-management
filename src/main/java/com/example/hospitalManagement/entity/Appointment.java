cfd79054b508d1463fa10e61fe6437557aabe8b0
Shivani Panjala <shivanimadhav4@gmail.com>
2025-10-24 17:14:52 +0530

    feat: many-to-one

---
+++ src/main/java/com/example/hospitalManagement/entity/Appointment.java
@@ -0,0 +1,26 @@
+package com.example.hospitalManagement.entity;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate appointmentTime;
+
+    @Column(length = 500)
+    private String reason;
+
+    /*
+         Defines a many-to-one relationship between Appointment and Patient.
+         Many appointments can be associated with one patient.
+         The @JoinColumn specifies the foreign key column ("patient_id") in the Appointment table
+         that references the primary key of the Patient entity.
+    */
+    @ManyToOne // Many Appointments to One Patient
+    @JoinColumn(name = "patient_id", nullable = false) //patient is required and not nullable
+    private Patient patient;
+}
